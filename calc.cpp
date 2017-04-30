4b3149b9ab55408307ac4606beec41dc5e39c487
Arindam Nath <arindam04@gmail.com>
2017-04-30 08:12:21 -0400

    adding calcd func

---
+++ calc.cpp
@@ -0,0 +1,14 @@
+
+namespace Calcs {
+	template <typename T> 
+	T add( const T &i1, const T& i2)
+	{
+		return i1 + i2;
+	}
+
+	template <typename T>
+	T mul(const T& i1, const T&i2)
+	{
+		return i *i2;
+	}
+}
